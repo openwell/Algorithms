fdedbc275feba03909bbed4b03d47c580405d2f0
Lambda <lambda@Lambdas-MacBook-Pro-3.local>
2019-10-10 16:37:46 +0100

    attempted stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
-
 import argparse
-
 def find_max_profit(prices):
-  pass
-
-
+   max_profit = prices[1] - prices[0]
+   for i in range(0, len(prices)-1):
+       profit = 0
+       for j in range(i+1, len(prices)-1):
+           if prices[i] < prices[j]:
+               profit = prices[j] - prices[i]
+               if profit > max_profit:
+                   max_profit = profit
+   return max_profit
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
-
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+   # This is just some code to accept inputs from the command line
+   parser = argparse.ArgumentParser(
+       description='Find max profit from prices.')
+   parser.add_argument('integers', metavar='N', type=int,
+                       nargs='+', help='an integer price')
+   args = parser.parse_args()
+   print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+       profit=find_max_profit(args.integers), prices=args.integers))
